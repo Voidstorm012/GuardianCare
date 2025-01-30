f8637dd209b1806039488c3a7cb2c712ad2f7f24
Syahmi <syahmisamri@gmail.com>
2025-01-31 03:03:59 +0800

    Optimized Imports

--- app/src/main/java/com/example/guardiancare/MainActivity.kt
+++ app/src/main/java/com/example/guardiancare/MainActivity.kt
@@ -4,12 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.example.guardiancare.ui.theme.GuardianCareTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/guardiancare/MainDrawerScreen.kt
+++ app/src/main/java/com/example/guardiancare/MainDrawerScreen.kt
@@ -2,8 +2,6 @@ package com.example.guardiancare
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -11,8 +9,25 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -22,12 +37,12 @@ import androidx.navigation.compose.rememberNavController
 import com.example.guardiancare.navigation.DrawerItem
 import com.example.guardiancare.navigation.Routes
 import com.example.guardiancare.ui.screens.FallDetectionScreen
-import kotlinx.coroutines.launch
 import com.example.guardiancare.ui.screens.HomeScreen
 import com.example.guardiancare.ui.screens.ProfileScreen
 import com.example.guardiancare.ui.screens.SettingsScreen
 import com.example.guardiancare.utils.BleManager
 import com.example.guardiancare.utils.NotificationHelper
+import kotlinx.coroutines.launch
 
 @RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
